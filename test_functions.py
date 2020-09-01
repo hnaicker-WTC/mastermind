bca746e20dddc55b65b2b9d72e42f267e74a62bd
hnaicker <hnaicker@student.wethinkcode.co.za>
2020-09-01 11:50:38 +0200

    wtc-lms save

--- test_functions.py
+++ test_functions.py
@@ -13,7 +13,7 @@ class TestMastermind(unittest.TestCase):
         for i in range (100):
             code = mastermind.create_code()
             is_valid_list = [element for element in code if str(element).isdigit() and element >= 1 and element <= 8] #checks there are only digits in code and digits are between 1 and 8
-            if len(code) == 4 and len(code) == len(is_valid_list) and len(code) == len(set(code)): # checks if code is 4 long, valid list is 4 long and there are no duplicates in list
+            if len(code) == 4 and len(code) == len(is_valid_list) and len(code) == len(set(code)): # checks if code is 4 long, checks if code input is a list of 4 numbers and there are no duplicates in list
                 code_is_valid = True
                   
         self.assertTrue(code_is_valid, "code should contain 4 digits in range 1-8, no duplicates.")
@@ -23,3 +23,5 @@ class TestMastermind(unittest.TestCase):
         code_is_correct = False
 
         
+# if __name__ == '__main__':
+#     unittest.main()
